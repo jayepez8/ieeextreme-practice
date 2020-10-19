76ceeba68f12cbe84aaebba18a9dbcee294a8ca1
jayepez8 <jayepez8@espe.edu.ec>
2020-10-19 02:26:32 -0500

    damn telescope

--- Shuffle/main.cpp
+++ Shuffle/main.cpp
@@ -14,12 +14,12 @@ int main(int argc, char const *argv[])
 {
     int n;
     char aux;
-    cin >> n >> ws;    
-    int **values=createMatrix(n);
+    cin >> n >> ws;
+    int **values = createMatrix(n);
     int *temp = createVector(n);
 
     for (int i = 0; i < n; i++)
-    {          
+    {
         values[i][i] = 0;
         string line;
         getline(cin, line);
@@ -30,21 +30,77 @@ int main(int argc, char const *argv[])
             values[i][number] = 0;
         }
     }
-    printMatrix(values, n);
-    int resFinal = 0;
-    int k=0;
-        for(int i=k;i<n;i++){
-        bool van=false;
-            for(int j=0;j<n;j++){
-                if(values[i][j]==1){
-                    temp[j]=1;
-                    van=true;
-                    k=i;
-                    i=k;
-                }                  
+    int pos = 0;
+    int cont = 0;
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (values[i][j] == 0)
+                cont++;
+            else
+                pos = j;
+        }
+        if (cont == n - 1)
+        {
+            for (int j = 0; j < n; j++)
+                values[j][pos] = 0;
+
+            if (temp[pos] == 0)
+            {
+                temp[pos] = 1;
             }
+            i = 0;
+            cont = 0;
+        }
+        else
+        {
+            cont = 0;
+        }
     }
-    //cout<<resFinal<<endl;
+    cont = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (values[j][i] == 0)
+                cont++;
+            else
+                pos = j;
+        }
+        if (cont == n - 1)
+        {
+            for (int j = 0; j < n; j++)
+                values[pos][j] = 0;
+
+            if (temp[i] == 0)
+            {
+                temp[i] = 1;
+            }
+            i = 0;
+            cont = 0;
+        }
+        else
+        {
+            cont = 0;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (values[j][i] == 0)
+                cont++;
+        }
+        if (cont == n && temp[i] == 0)
+        {
+            res++;
+        }
+
+        cont = 0;
+    }
+    cout << res << endl;
     return 0;
 }
 
@@ -62,10 +118,11 @@ int **createMatrix(int n)
     return matrix;
 }
 
-int *createVector(int n){
-    int *vector = new int [n];
-    for(int i=0;i<n;i++)
-        vector[i]=0;
+int *createVector(int n)
+{
+    int *vector = new int[n];
+    for (int i = 0; i < n; i++)
+        vector[i] = 0;
     return vector;
 }
 
@@ -79,6 +136,4 @@ void printMatrix(int **matrix, int size)
         }
         cout << endl;
     }
-    
 }
-
--- Telescope_Scheduling/main.cpp
+++ Telescope_Scheduling/main.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -22,12 +20,30 @@ void copyCad(int a[][3], int b[][3], int p1, int p2)
     }
 }
 
-void orderByEntry(int stars[][3], int n)
+void orderByPriory(int stars[][3], int n)
 {
     int aux[n][3];
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n - 1; j++)
+        {
+            //if (stars[j][1] > stars[j + 1][1])
+            if (stars[j][2] < stars[j + 1][2])
+            {
+                copyCad(aux, stars, j, j);
+                copyCad(stars, stars, j, j + 1);
+                copyCad(stars, aux, j + 1, j);
+            }
+        }
+    }
+}
+
+void orderByInput(int stars[][3], int n, int s, int f)
+{
+    int aux[n][3];
+    for (int i = s; i < f; i++)
+    {
+        for (int j = s; j < f - 1; j++)
         {
             if (stars[j][0] > stars[j + 1][0])
             {
@@ -38,6 +54,114 @@ void orderByEntry(int stars[][3], int n)
         }
     }
 }
+
+void orderFinal(int stars[][3], int n, int priStars[], int tamPriory)
+{
+    int aux = 0;
+    for (int i = 0; i < tamPriory; i++)
+    {
+        orderByInput(stars, n, aux, priStars[i] + 1);
+        aux = priStars[i] + 1;
+    }
+}
+
+int contPriory(int stars[][3], int n)
+{
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (stars[i][2] != stars[i + 1][2])
+            res++;
+    }
+    return res;
+}
+
+void posStars(int stars[][3], int n, int priStars[])
+{
+    int aux = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (stars[i][2] != stars[i + 1][2])
+        {
+            priStars[aux] = i;
+            aux++;
+        }
+    }
+}
+
+int tamCad(int matrix[][3], int n)
+{
+    int res = 0;
+    for (int i = 0; i < n; i++)
+        if (matrix[i][2] != 0)
+            res++;
+    return res;
+}
+
+int prosess(int stars[][3], int n, int index)
+{
+    int res = 0;
+    int cont = 0;
+    int sol[n][3];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 3; j++)
+            sol[i][j] = 0;
+    }
+    copyCad(sol, stars, 0, 0);
+    int tamSol = tamCad(sol, n);
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j <= tamSol; j++)
+        {
+            if (stars[i][0] > sol[j][1])
+            {
+                if (sol[j + 1][0] == 0)
+                {
+                    copyCad(sol, stars, j + 1, i);
+                    cont++;
+                    break;
+                }
+                else if (stars[i][1] < sol[j + 1][0])
+                {
+                    for (int k = j + 1; k < tamCad(sol, n); k++)
+                    {
+                        copyCad(sol, sol, k + 1, k);
+                    }
+                    copyCad(sol, stars, j + 1, i);
+                    cont++;
+                    break;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i <= cont; i++)
+        res += sol[i][2];
+    return res;
+}
+
+/*
+int prosess(int stars[][3], int n, int index)
+{
+    int res = stars[index][2];
+    for (int i = index; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (stars[i][1] < stars[j][0])
+            {
+                res += stars[j][2];
+                i = j - 1;
+                break;
+            }
+            if (j == n - 1)
+                i = n;
+        }
+    }
+    return res;
+}
+*/
 
 int main(void)
 {
@@ -50,20 +174,31 @@ int main(void)
         cin >> stars[i][0] >> stars[i][1] >> stars[i][2];
     }
 
-    orderByEntry(stars, n);
-    int res = stars[0][2];
+    orderByPriory(stars, n);
+
+    int tamPriory = contPriory(stars, n);
+    int priStars[tamPriory];
+    posStars(stars, n, priStars);
+    orderFinal(stars, n, priStars, tamPriory);
+
+    cout << prosess(stars, n, tamPriory);
+
+    /*
+    orderByPriory(stars, n);    
+    int sol[n];
+
     for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < n; j++)
-        {
-            if (stars[i][1] < stars[j][0])
-            {
-                res += stars[i][2];
-                i = j;
-            }
-        }
+        sol[i] = prosess(stars, n, i);
+    }
+
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (sol[i] > res)
+            res = sol[i];
     }
     cout << res;
-
+*/
     return 0;
 }
